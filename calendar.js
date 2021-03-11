303f1befb1c27a4402f78dada73f2adf5261a307
Mungun2227 <78751955+Mungun2227@users.noreply.github.com>
2021-03-11 19:20:30 +0800

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,94 +1,97 @@
-var monthNamesRy = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var daysOfTheWeekRy = ["S", "M", "T", "W", "T", "F", "S"]
-var d = new Date();
-var year = d.getFullYear();
-var thisMonth = d.getMonth();
-var today = d.getDate("Өнөөдөр");
-var year = d.getFullYear();
-for( var month = 0; month< 12; month++){
-	createCalendar(month);
-}
-function createCalendar(month){
-    var monthDiv = createMonthHeader(month);
-    var firstDayOfTheMonth  = getFirstDayOfTheMonth(year,month);
-    var daysinmonth  = daysInMonth(year,month)
-    var counter = 0,order = 6;
-    for( var i = 0; i < firstDayOfTheMonth +7; i++){
-    order++;
-    createDay(month, "&nbsp;",order, monthDiv);
+var calendar2021 = {
+    jan: { 1: 'Сайхан амарна' },
+    feb: {   1: 'Сагсны тэмцээнтэй', 3: 'Шагнал гардуулна даа',  17: 'Жавхлан багшийн лаб 2-ыг хийнэ',   },
+    mar: {  2: 'Энэ лабынхаа хугацааг сунгах уу яах вэ гэдэгээ шийднэ', 6: 'Энд юу бичье дээ байз', 8: 'Эмэгтэйчүүддээ баяр хүргэнэ дээ', },
+    apr: { 1: 'Бүгдээрээ худлаа ярьцаагаагаарай' },
+    may: { 10: 'Энэ сард ч ёстой юу ч болдоггүй сар даа' },
+    jun: { 6: 'Жавхлан багшийн төрсөн өдөр' },
+    jul: { 4: 'Хичээл амарсаан ураа' },
+    aug: { 1: 'Хөдөө явдаг цаг даа', 25: 'Хичээл сонголт эхэллээ' },
+    sep: { 1: '9-н сарын нэгэн боллоо ерөөсөө бидний баяр даа' },
+    oct: { 13: 'Сур сур бас дахин сур' },
+    nov: { 2: 'Сурсаар л бай' },
+    dec: { 20: 'Өвлийн семистер хаагдах нь дээ', 30: 'Дүн гаргаж дууслаа баярлалаа баяртай',},
+  }; //events zarlah yavts    
+  var monthNames = [  'January',   'February',   'March',   'April',   'May',   'June',   'July',  'August',   'September', 'October',  'November', 'December', //month names olgoh yavts
+  ];
+  var date = new Date(); //date object uusgeh uil yavts
+  function searchEvents() {} //hailt hiih 
+  function printCalendar() {    //calendar hevleh
+    let days = ''; //udur nemeh
+    let events = ''; //events nemeh
+    let monthName = monthNames[date.getMonth()].substring(0, 3).toLowerCase(); //sariin ner uguh
+  
+    let dayOfPrevMonth = new Date(date.getFullYear(), date.getMonth(), 0); //umnuh sariin udruudiig avah
+  
+    let dayofCurrentMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0); //ene sariin udruudiig avah
+    document.querySelector('.month h1').innerHTML = monthNames[date.getMonth()];
+    for (let i = 0; i < monthNames; i++) {} //monthNames func hurtel sar uusgeh
+    for (let i = 1; i <= dayOfPrevMonth.getDay(); i++) { //sariin udur uusgeh
+      days += `<div>${
+        dayOfPrevMonth.getDate() - dayOfPrevMonth.getDay() + i
+      }</div>`;
     }
-    for( var i = firstDayOfTheMonth; i < daysInMonth(year,month) + firstDayOfTheMonth; i++){
-    counter ++;
-    order++;
-    createDay(month,counter,order,monthDiv);
+    for (let i = 1; i <= dayofCurrentMonth.getDate(); i++) { //ene sariin calendar 
+      if (calendar2021[monthName][i]) { //herev sariin udur n deer zarlsan eventiin udurtei tentsvel
+        if (date.getFullYear() == 2021) { 
+          events += `<div>${date.getMonth() + 1} сарын ${i} - ${
+            calendar2021[monthName][i]
+          }</div>`;
+          days += `<div class="currentMonth event">${i}</div>`; 
+        } else {
+          days += `<div class="currentMonth">${i}</div>`;
+        }
+      } else {
+        days += `<div class="currentMonth">${i}</div>`;
+      }
     }
-    for( var i = firstDayOfTheMonth + daysinmonth; i < 6*7; i++){
-    order++;
-    createDay(month, "&nbsp;",order, monthDiv);
+  
+    for (let i = 1; i <= 7 - dayofCurrentMonth.getDay(); i++) {
+      days += `<div>${i}</div>`;
     }
-}
-    function createDay(month,counter,order,monthDiv){
-    var day = document.createElement("div");
-    if(month == thisMonth && counter == today){day.setAttribute("class","to day");
+    document.querySelector('.days').innerHTML = days;
+    document.querySelector('.listed-events').innerHTML = events;
+  }
+  printCalendar();
+  function daraasar() {
+    date.setMonth(date.getMonth() + 1);
+    printCalendar();
+  }
+  function umnuhsar() {
+    date.setMonth(date.getMonth() - 1);
+    printCalendar();
+  }
+  document.querySelector('.search-input').addEventListener('keypress', (e) => {
+    let searchResults = '',
+      count = 0;
+    if (e.key === 'Enter') {
+      if (document.querySelector('.search-input').value == '') {
+        searchResults += '<h3>Not Found</h3>';
+      } else {
+        searchResults += '<h3>Search Results</h3><div class="result">';
+        for (let i = 0; i < monthNames.length; i++) {
+          let monthName = monthNames[i].substring(0, 3).toLowerCase();
+          for (let j = 1; j <= 31; j++) {
+            if (calendar2021[monthName][j]) {
+              if (
+                calendar2021[monthName][j].includes(
+                  document.querySelector('.search-input').value
+                )
+              ) {
+                count++;
+                searchResults += `<div>${i + 1} Сарын ${j} - ${
+                  calendar2021[monthName][j]
+                }</div>`;
+              }
+            }
+          }
+        }
+      }
+      searchResults += '</div>';
+      if (count === 0) {
+        searchResults = '<h3>Not Found</h3>';
+      }
+      document.querySelector('.search-result').innerHTML = searchResults;
+      console.log(searchResults);
     }
-    else{
-    day.setAttribute("class","day");}
-    day.setAttribute("style","order:"+order);
-    day.innerHTML = counter;
-    monthDiv.appendChild(day);
-    }
-    function createMonthHeader(month){
-        var calendar = document.querySelector(".calendar");
-            
-    var monthDiv = document.createElement("div");
-        monthDiv.setAttribute("class","month");
-        calendar.appendChild(monthDiv);
-        
-    var h4 = document.createElement("h4");
-        h4.innerHTML = monthNamesRy[month];
-        monthDiv.appendChild(h4);	
-        
-    for( var i = 0; i < 7; i++){
-        var hday = document.createElement("div");
-        hday.setAttribute("class","day OfWeek");
-        hday.setAttribute("style","order:"+i);
-        hday.innerHTML = daysOfTheWeekRy[i].toUpperCase();
-        monthDiv.appendChild(hday);
-    }
-        return monthDiv;	
-    }
-    function daysInMonth(year,month) {
-        return new Date(year, month+1, 0).getDate();
-    }
-    function getMonthName(month){
-        return monthNamesRy[month];
-    }
-    function getDayName(day){
-        return daysOfTheWeekRy[day];
-    }
-    function getFirstDayOfTheMonth(y,m){
-        var firstDay = new Date(y, m, 1);
-        return firstDay.getDay();
-    }
-    function getLastDayOfTheMonth(y,m){
-        var lastDay = new Date(y, m + 1, 0);
-        return lastDay.getDay();
-    }
-
-    var calendar2021 = {
-        Jan: {1: "Сайхан амарна"},
-        Feb: {1: "Сагсны тэмцээнтэй", 3: "Шагнал гардуулна даа", 17: "Жавхлан багшийн лаб 2-ыг хийнэ"},
-        Mar: {2: "Энэ лабыг сунгах эсэхээ шийднэ", 6: "Энд юу бичье дээ байз", 8: "Эмэгтэйчүүддээ баяр хүргэнэ"},
-        Apr: {1: "Бүгдээрээ худлаа ярьцгаана"},
-        May: {10: "Энэ сард ёстой юу ч болдоггүй сар даа"},
-        Jun: {6: "Жавхлан багшийн төрсөн өдөр", 30: "Мөнгөөнөөгийн төрсөн өдөр"},
-        Jul: {4: "Хичээл амарсаан"},
-        Aug: {1: "Аяллаар явна даа", 25: "Хичээл сонголт эхэллээ дээ"},
-        Sep: {1: "9 сарын нэгэн боллоо ерөөсөө бидний баяр даа"},
-        Oct: {13: "Сур сур бас дахин сур"},
-        Nov: {2: "Сурсаар л бай"},
-        Dec: {20: "Өвлийн семистер хаагдах нь дээ", 30: "Дүн гаргаж дууслаа баярлалаа баяртай"}
-    }
-    
-    
-            
+  });
