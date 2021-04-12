b447eafee40ce83adbab8a4c841916a8cd1751ff
Mungun2227 <78751955+Mungun2227@users.noreply.github.com>
2021-04-12 13:41:06 +0800

    Update header.js

--- Project 4/components/header/header.js
+++ Project 4/components/header/header.js
@@ -5,6 +5,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
+            <img src="https://i.redd.it/j7bxya20ems61.jpg" alt="Hello"/>
                 <a href="https://www.youtube.com/watch?v=ih2xubMaZWI" className="hidden-msg">
                     <h1>Click Here</h1>
                 </a>
